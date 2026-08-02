327c9ab2dd3d4529e08ddaa76bde373f12b089eb
itaygoldenberg <itaygold11@gmail.com>
2026-08-02 23:53:46 +0300

    Fix production API routing

--- api/ai/advice.js
+++ api/ai/advice.js
@@ -1,4 +1,4 @@
-import { handleApiRequest } from "../../[...path].js";
+import { handleApiRequest } from "../[...path].js";
 
 export default function handler(request, response) {
     return handleApiRequest(request, response, "/ai/advice");
